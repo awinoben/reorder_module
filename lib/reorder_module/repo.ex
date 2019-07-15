5b48c2bf2b6cddeb121774e03a28b49008a263fe
Ben <papasofts@gmail.com>
2019-07-15 19:19:01 +0300

    updates

--- lib/reorder_module/repo.ex
+++ lib/reorder_module/repo.ex
@@ -1,7 +1,7 @@
 defmodule ReorderModule.Repo do
   use Ecto.Repo,
   otp_app: :reorder_module,
-  adapter: Ecto.Adapters.Postgres,
+  adapter: Ecto.Adapters.Postgres
 
   @doc """
   Dynamically loads the repository url from the
