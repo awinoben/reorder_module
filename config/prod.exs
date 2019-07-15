3c7149414441cc1a295f28d222dd345c73dd2fe1
Ben <papasofts@gmail.com>
2019-07-15 16:49:02 +0300

    initial commit

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :reorder_module, ReorderModuleWeb.Endpoint,
   load_from_system_env: true,
   # url: [host: "example.com", port: 80],
-  url: [scheme: "https", host: "enigmatic-temple-56655", port: 443],
+  url: [scheme: "https", host: "vast-shelf-52248", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
