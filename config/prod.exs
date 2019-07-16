7366c58067ae3a959962aff15c06cbab80364a4d
Ben <papasofts@gmail.com>
2019-07-16 10:16:32 +0300

    Added heroku deploy configs

--- config/prod.exs
+++ config/prod.exs
@@ -16,9 +16,11 @@ use Mix.Config
 config :reorder_module, ReorderModuleWeb.Endpoint,
   load_from_system_env: true,
   # url: [host: "example.com", port: 80],
-  url: [scheme: "https", host: "sleepy-escarpment-64829", port: 443],
-  #force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "sleepy-escarpment-64829.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
--- lib/reorder_module/repo.ex
+++ lib/reorder_module/repo.ex
@@ -1,8 +1,6 @@
 defmodule ReorderModule.Repo do
-  use Ecto.Repo,
-  otp_app: :reorder_module,
-  adapter: Ecto.Adapters.Postgres
-
+  use Ecto.Repo, otp_app: :reorder_module
+  #alias Ecto.Adapters.Postgres
 
   @doc """
   Dynamically loads the repository url from the
